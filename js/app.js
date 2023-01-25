1eadb876be001c9bab5ea17e97c8969a84e9d3c7
Samir Kattel <samirdon963@gmail.com>
2023-01-25 21:30:15 +0545

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//This function is called directly via the alternation of the body tag
+function updateTime(){
+    let currentDate = new Date();
+    var cHour = currentDate.getHours();
+    var cMin = currentDate.getMinutes();
+    var cSec = currentDate.getSeconds();
+    var ampm = cHour>12?"PM":"AM";
+    cHour = cHour>12?cHour-12:cHour;
+    cHour = cHour<10?"0"+cHour:cHour;
+    cMin = cMin<10?"0"+cMin:cMin;
+    cSec = cSec<10?"0"+cSec:cSec;
+    let currentTime = document.querySelector('.current-time');
+    currentTime.innerText = cHour+":"+cMin+":"+cSec+ampm;
+}
